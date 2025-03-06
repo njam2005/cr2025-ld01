ea3e8e009d9d8cdb3ae781c47f6eab1617caf97c
user <user@DESKTOP-31AKU8T>
2025-03-06 10:13:01 +0700

    2702331473 - Muhammad Nizam Al Zahrawi

--- src/exercise/ExtractMethod.java
+++ src/exercise/ExtractMethod.java
@@ -18,25 +18,34 @@ public class ExtractMethod {
 		orders = new Order();
 	}
 	// TODO: reduce this method with extract method
+	
 	void printOwing() {
-	  Enumeration elements = orders.elements();
-	  double outstanding = 0.0;
-
-	  // print banner
-	  System.out.println ("*****************************");
-	  System.out.println ("****** Customer totals ******");
-	  System.out.println ("*****************************");
-
-	  // print owings
-	  while (elements.hasMoreElements()) {
-	    Order each = (Order) elements.nextElement();
-	    outstanding += each.getAmount();
-	  }
-
-	  // print details
-	  System.out.println("name: " + name);
-	  System.out.println("amount: " + outstanding);
+		printBanner();
+		double outstanding = owings();
+		printDetails(outstanding);
 	}
+	
+	public void printBanner{
+		  System.out.println ("*****************************");
+		  System.out.println ("****** Customer totals ******");
+		  System.out.println ("*****************************");
+	}
+	
+	public double owings(){
+		Enumeration elements = orders.elements();
+		  double outstanding = 0.0;
+		  while (elements.hasMoreElements()) {
+			    Order each = (Order) elements.nextElement();
+			    outstanding += each.getAmount();
+			  }
+		  return outstanding;
+	}
+	
+	public void printDetails(double outstanding) {
+		  System.out.println("name: " + name);
+		  System.out.println("amount: " + outstanding);
+	}
+	
 	
 	class Order implements Enumeration {
 		private double [] amounts;
